fdb02369c8e17dbee3be7f2e6e6129286525fe4a
Andres Osante <andres.osante@hint.com>
2022-06-14 22:39:11 -0600

    checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'archives/index'
   resources :entries
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "entries#index"
